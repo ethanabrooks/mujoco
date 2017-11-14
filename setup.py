b2b3583d814eb2cfe371c9223182b29333bb2d49
Ethan Brooks <ethanabrooks@gmail.com>
2017-11-14 10:01:32 -0500

    fixed up setup.py to build to build/

--- setup.py
+++ setup.py
@@ -5,30 +5,31 @@ from Cython.Build import cythonize
 from os.path import join, expanduser
 
 mjpro_path = join(expanduser('~'), '.mujoco', 'mjpro150')
+build_dir = "build"
 
 extensions = Extension(
-        'mujoco',
-        sources=[
-            "pyx/mujoco.pyx",
-            "src/render.c",
-            ],
-        include_dirs=[
-            join(mjpro_path, 'include'),
-            'headers',
-            'pxd',
-            ],
-        libraries=['mujoco150', 'GL', 'glew'],
-        library_dirs=[join(mjpro_path, 'bin')],
-        extra_compile_args=[
-            '-fopenmp',  # needed for OpenMP
-            '-w',  # suppress numpy compilation warnings
-            ],
-        extra_link_args=['-fopenmp', join(mjpro_path, 'bin', 'libglfw.so.3')],
-        language='c')
+    join(build_dir, 'mujoco'),
+    sources=[
+        "pyx/mujoco.pyx",
+        "src/render.c",
+    ],
+    include_dirs=[
+        join(mjpro_path, 'include'),
+        'headers',
+        'pxd',
+    ],
+    libraries=['mujoco150', 'GL', 'glew'],
+    library_dirs=[join(mjpro_path, 'bin')],
+    extra_compile_args=[
+        '-fopenmp',  # needed for OpenMP
+        '-w',  # suppress numpy compilation warnings
+    ],
+    extra_link_args=['-fopenmp',
+                     join(mjpro_path, 'bin', 'libglfw.so.3')],
+    language='c')
 
 if __name__ == '__main__':
-    setup(
-            ext_modules=cythonize(extensions,
-            build_dir="build",
-                ),
-            )
+    setup(ext_modules=cythonize(
+        extensions,
+        build_dir=build_dir,
+    ))
