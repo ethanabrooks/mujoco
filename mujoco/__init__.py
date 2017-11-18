cef2f86509963b2e1fb4054ba250ed7e342cb421
Ethan Brooks <ethanabrooks@gmail.com>
2017-11-17 21:09:56 -0500

    just swapped in Sim for BaseSim and it worked!

--- mujoco/__init__.py
+++ mujoco/__init__.py
@@ -3,10 +3,9 @@
 import mujoco
 from mujoco.sim import GeomType, ObjType, BaseSim
 print('dir(mujoco.simGlfw)', dir(mujoco))
-# import mujoco.simGlfw
+from mujoco.simGlfw import Sim
 # from mujoco.simGlfw import Sim
 
-Sim = BaseSim
 
 # Public API:
 __all__ = ['Sim', 'GeomType', 'ObjType', '__version__']
