17eb142e82f286b1e21e49ef5f47c0851baf3edc
Ethan Brooks <ethanabrooks@gmail.com>
2017-11-19 16:53:34 -0500

    added mujoco to mocks

--- docs/conf.py
+++ docs/conf.py
@@ -27,7 +27,7 @@ class Mock(MagicMock):
     def __getattr__(cls, name):
             return MagicMock()
 
-MOCK_MODULES = ['numpy', 'cython']
+MOCK_MODULES = ['numpy', 'cython', 'mujoco']
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 
