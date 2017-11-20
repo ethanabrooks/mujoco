59aeac80d5101cdf57d8801945879cff222dd7c6
Ethan Brooks <ethanabrooks@gmail.com>
2017-11-20 15:35:08 -0500

    fixed simGlfw bug

--- mujoco/__init__.py
+++ mujoco/__init__.py
@@ -1,6 +1,8 @@
 import os
+import sys
+
 from mujoco.sim import GeomType, ObjType
-if os.environ.get('EGL'):
+if sys.platform in ['linux', 'linux2'] and not os.environ.get('RENDER'):
     from mujoco.simEgl import Sim
 else:
     from mujoco.simGlfw import Sim
