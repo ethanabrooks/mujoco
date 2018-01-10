785bc31dc2f9b0f0850127c4cd5a50f8d9e4a5c2
Ethan Brooks <ethanabrooks@gmail.com>
2018-01-10 14:49:22 -0500

    increment version number

--- setup.py
+++ setup.py
@@ -104,7 +104,7 @@ if __name__ == '__main__':
 
     setup(
         name='mujoco',
-        version='2.0.0',
+        version='2.0.1',
         description='Python wrapper for MuJoCo physics simulation.',
         long_description=long_description,
         url='https://github.com/lobachevzky/mujoco',
