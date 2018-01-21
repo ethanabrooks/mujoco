6b55c45e33b55decbd2350c134940fa1e0b3c3bf
Ethan Brooks <ethanabrooks@gmail.com>
2018-01-20 19:21:08 -0500

    works

--- src/renderOffscreenBug.c
+++ src/renderOffscreenBug.c
@@ -1,153 +1,105 @@
-#include "util.h"
-#ifdef MJ_EGL
-#include "utilEgl.h"
-#else
-#include "utilGlfw.h"
-#endif
 #include "mujoco.h"
 #include "stdio.h"
 #include "stdlib.h"
 #include "string.h"
+#include "glfw3.h"
 
-int initMujoco(const char *filepath, State * state)
-{
-	char error[1000] = "Could not load xml model";
-	state->m = mj_loadXML(filepath, 0, error, 1000);
-	if (!state->m)
-		mju_error_s("Load model error: %s", error);
-	state->d = mj_makeData(state->m);
 
-	mj_forward(state->m, state->d);
-	mjv_makeScene(&state->scn, 1000);
-	mjv_defaultCamera(&state->cam);
-	mjv_defaultOption(&state->opt);
-	mjr_defaultContext(&state->con);
-	mjr_makeContext(state->m, &state->con, 200);
-	return 0;
-}
+//-------------------------------- global data ------------------------------------------
 
-int setCamera(int camid, State * state)
-{
-	mjvScene *scn = &(state->scn);
-	mjvCamera *cam = &(state->cam);
-	mjvOption *opt = &(state->opt);
+// MuJoCo model and data
+mjModel* m = 0;
+mjData* d = 0;
 
-	cam->fixedcamid = camid;
-	if (camid == -1) {
-		cam->type = mjCAMERA_FREE;
-	} else {
-		cam->type = mjCAMERA_FIXED;
-	}
+// MuJoCo visualization
+mjvScene scn;
+mjvCamera cam;
+mjvOption opt;
+mjrContext con;
 
-	mjv_updateScene(state->m, state->d, opt, NULL, cam, mjCAT_ALL, scn);
-	return 0;
-}
-
-int
-renderOffscreen(unsigned char *rgb,
-		int height, int width, State * state)
-{
-	mjvScene scn = state->scn;
-	mjrContext con = state->con;
-	mjrRect viewport = { 0, 0, height, width };
-
-	// write offscreen-rendered pixels to file
-	mjr_setBuffer(mjFB_OFFSCREEN, &con);
-	if (con.currentBuffer != mjFB_OFFSCREEN)
-		printf
-		    ("Warning: offscreen rendering not supported, using default/window framebuffer\n");
-	mjr_render(viewport, &scn, &con);
-	mjr_readPixels(rgb, NULL, viewport, &con);
-	return 0;
-}
-
-int closeMujoco(State * state)
-{
-	mjvScene scn = state->scn;
-	mjrContext con = state->con;
-
-	mj_deleteData(state->d);
-	mj_deleteModel(state->m);
-	mjr_freeContext(&con);
-	mjv_freeScene(&scn);
-	mj_deactivate();
-	return 0;
-}
-
-int count_zeros(unsigned char *rgb, size_t size) {
-  int count = 0;
-  for (size_t i = 0; i < size; i++) {
-    if (rgb[i] == 0) {
-      count++;
-    }
-  }
-  return count;
-}
 
 //-------------------------------- main function ----------------------------------------
 
-int main(int argc, const char **argv)
+int main(int argc, const char** argv)
 {
-	int H = 1024;
-	int W = 1024;
-  /*char const *filepath = "../zero_shot/environment/models/pick-and-place/world.xml"; */
-  char const *filepath = "xml/humanoid.xml";
-	char const *keypath = "../.mujoco/mjkey.txt";
-	State state;
-#ifdef MJ_EGL
-	initOpenGL();
-#else
-	GraphicsState graphicsState;
-	initOpenGL(&graphicsState, &state);
-#endif
-	mj_activate(keypath);
-	// install GLFW mouse and keyboard callbacks
-	initMujoco(filepath, &state);
-	mj_resetDataKeyframe(state.m, state.d, 0);
+    if( !glfwInit() )
+        mju_error("Could not initialize GLFW");
 
-	// allocate rgb and depth buffers
-	unsigned char *rgb = (unsigned char *)malloc(3 * H * W);
-	if (!rgb)
-		mju_error("Could not allocate buffers");
+    // create visible window, double-buffered
+    glfwWindowHint(GLFW_VISIBLE, GLFW_TRUE);
+    glfwWindowHint(GLFW_DOUBLEBUFFER, GLFW_TRUE);
+    GLFWwindow* window = glfwCreateWindow(800, 800, "Visible window", NULL, NULL);
+    if( !window )
+        mju_error("Could not create GLFW window");
 
-	// create output rgb file
-	FILE *fp = fopen("build/rgb.out", "wb");
-	if (!fp)
-		mju_error("Could not open rgbfile for writing");
+    glfwMakeContextCurrent(window);
+    mj_activate("../.mujoco/mjkey.txt");
 
-	// main loop
-	for (int i = 0; i < 10; i++) {
-    setCamera(-1, &state);
-		renderOffscreen(rgb, H, W, &state);
-		fwrite(rgb, 3, H * W, fp);
-#ifndef MJ_EGL
-    float pos1[] = {0, 0, 0};
-    float pos2[] = {0.2, 0, 0};
+    char error[1000] = "Could not load xml model";
+    m = mj_loadXML("xml/humanoid.xml", 0, error, 1000);
+    if( !m )
+        mju_error_s("Load model error: %s", error);
+    d = mj_makeData(m);
+    mj_forward(m, d);
 
-    setCamera(0, &state);
-    addLabel("1\n", pos1, &state);
-    addLabel("2\n", pos2, &state);
-		renderOnscreen(&graphicsState);
-#endif
-		state.d->ctrl[0] = 0.5;
-		mj_step(state.m, state.d);
-	}
-	printf
-	    ("ffmpeg -f rawvideo -pixel_format rgb24 -video_size %dx%d -framerate 60 -i build/rgb.out -vf 'vflip' build/video.mp4\n",
-	     H, W);
-  FILE *fout = fopen("rgb.pbm", "wb");
-	if (!fout)
-		mju_error("Could not open image file for writing");
+    // initialize MuJoCo visualization
+    mjv_makeScene(&scn, 1000);
+    mjv_defaultCamera(&cam);
+    mjv_defaultOption(&opt);
+    mjr_defaultContext(&con);
+    mjr_makeContext(m, &con, 200);
 
-  fprintf(fout, "P6\n%d %d\n255\n", W, H);
-  fwrite(rgb, 1, W * H * 3, fout);
+    // get size of window
+    mjrRect window_rect = {0, 0, 0, 0};
+    glfwGetFramebufferSize(window, &window_rect.width, &window_rect.height);
+    int W = window_rect.width;
+    int H = window_rect.height;
 
-	fclose(fout);
+    // allocate rgb and depth buffers
+    unsigned char* rgb = (unsigned char*)malloc(3*W*H);
+    if( !rgb )
+        mju_error("Could not allocate buffers");
 
-  printf("zeros: %d\n", count_zeros(rgb, 3 * H * W));
-	fclose(fp);
-	free(rgb);
-	closeMujoco(&state);
+    // create output rgb file
+    FILE* fp = fopen("rgb.out", "wb");
+    if( !fp )
+        mju_error("Could not open rgbfile for writing");
 
-	return 0;
+    // main loop
+    for( int i = 0; i < 50; i++) {
+      cam.fixedcamid = 0;
+      cam.type = mjCAMERA_FIXED;
+      mjv_updateScene(m, d, &opt, NULL, &cam, mjCAT_ALL, &scn);
+
+      // write offscreen-rendered pixels to file
+      mjr_render(window_rect, &scn, &con);
+      mjr_readPixels(rgb, NULL, window_rect, &con);
+      fwrite(rgb, 3, W*H, fp);
+
+      cam.fixedcamid = -1;
+      cam.type = mjCAMERA_FREE;
+      mjv_updateScene(m, d, &opt, NULL, &cam, mjCAT_ALL, &scn);
+      mjr_render(window_rect, &scn, &con);
+
+      glfwSwapBuffers(window);
+      mj_step(m, d);
+    }
+
+    FILE *fout = fopen("rgb.pbm", "wb");
+    if (!fout)
+      mju_error("Could not open image file for writing");
+
+    fprintf(fout, "P6\n%d %d\n255\n", W, H);
+    fwrite(rgb, 1, W * H * 3, fout);
+
+    fclose(fout);
+
+    fclose(fp);
+    free(rgb);
+    mj_deleteData(d);
+    mj_deleteModel(m);
+    mjr_freeContext(&con);
+    mjv_freeScene(&scn);
+    mj_deactivate();
+    return 0;
 }
