025dccb45690ab67eb584dec671b025583483663
Ethan Brooks <ethanabrooks@gmail.com>
2018-06-24 18:58:45 -0400

    indent

--- src/utilEgl.c
+++ src/utilEgl.c
@@ -3,11 +3,12 @@
 #include "utilEgl.h"
 #include "util.h"
 
-// create OpenGL context/window
-int initOpenGL()
+// create OpenGL context / window
+int 
+initOpenGL()
 {
-	// desired config
-	const EGLint configAttribs[] = {
+	//desired config
+	const EGLint	configAttribs[] = {
 		EGL_RED_SIZE, 8,
 		EGL_GREEN_SIZE, 8,
 		EGL_BLUE_SIZE, 8,
@@ -20,70 +21,72 @@ int initOpenGL()
 		EGL_NONE
 	};
 
-	// get default display
-	EGLDisplay eglDpy = eglGetDisplay(EGL_DEFAULT_DISPLAY);
+	//get default display
+		EGLDisplay eglDpy = eglGetDisplay(EGL_DEFAULT_DISPLAY);
 	if (eglDpy == EGL_NO_DISPLAY)
 		mju_error_i("Could not get EGL display, error 0x%x\n",
 			    eglGetError());
 
-	// initialize
-	EGLint major, minor;
+	//initialize
+		EGLint major, minor;
 	if (eglInitialize(eglDpy, &major, &minor) != EGL_TRUE)
 		mju_error_i("Could not initialize EGL, error 0x%x\n",
 			    eglGetError());
 
-	// choose config
-	EGLint numConfigs;
-	EGLConfig eglCfg;
+	//choose config
+		EGLint numConfigs;
+	EGLConfig	eglCfg;
 	if (eglChooseConfig(eglDpy, configAttribs, &eglCfg, 1, &numConfigs) !=
 	    EGL_TRUE)
 		mju_error_i("Could not choose EGL config, error 0x%x\n",
 			    eglGetError());
 
-	// bind OpenGL API
-	if (eglBindAPI(EGL_OPENGL_API) != EGL_TRUE)
+	//bind OpenGL API
+		if (eglBindAPI(EGL_OPENGL_API) != EGL_TRUE)
 		mju_error_i("Could not bind EGL OpenGL API, error 0x%x\n",
 			    eglGetError());
 
-	// create context
-	EGLContext eglCtx =
-	    eglCreateContext(eglDpy, eglCfg, EGL_NO_CONTEXT, NULL);
+	//create context
+		EGLContext eglCtx =
+		eglCreateContext(eglDpy, eglCfg, EGL_NO_CONTEXT, NULL);
 	if (eglCtx == EGL_NO_CONTEXT)
 		mju_error_i("Could not create EGL context, error 0x%x\n",
 			    eglGetError());
 
-	// make context current, no surface (let OpenGL handle FBO)
-	if (eglMakeCurrent(eglDpy, EGL_NO_SURFACE, EGL_NO_SURFACE, eglCtx) !=
-	    EGL_TRUE)
+	//make context current, no surface(let OpenGL handle FBO)
+		if (eglMakeCurrent(eglDpy, EGL_NO_SURFACE, EGL_NO_SURFACE, eglCtx) !=
+		    EGL_TRUE)
 		mju_error_i("Could not make EGL context current, error 0x%x\n",
 			    eglGetError());
 	return 0;
 }
 
-// close OpenGL context/window
-int closeOpenGL(void)
+//close OpenGL context / window
+int 
+closeOpenGL(void)
 {
-	// get current display
+	//get current display
 	EGLDisplay eglDpy = eglGetCurrentDisplay();
 	if (eglDpy == EGL_NO_DISPLAY)
 		return 1;
 
-	// get current context
-	EGLContext eglCtx = eglGetCurrentContext();
+	//get current context
+		EGLContext eglCtx = eglGetCurrentContext();
 
-	// release context
-	eglMakeCurrent(eglDpy, EGL_NO_SURFACE, EGL_NO_SURFACE, EGL_NO_CONTEXT);
+	//release context
+		eglMakeCurrent(eglDpy, EGL_NO_SURFACE, EGL_NO_SURFACE, EGL_NO_CONTEXT);
 
-	// destroy context if valid
-	if (eglCtx != EGL_NO_CONTEXT)
-		eglDestroyContext(eglDpy, eglCtx);
+	//destroy context if valid
+		if (eglCtx != EGL_NO_CONTEXT)
+			eglDestroyContext(eglDpy, eglCtx);
 
-	// terminate display
-	eglTerminate(eglDpy);
+	//terminate display
+		eglTerminate(eglDpy);
 	return 0;
 }
 
-int renderOnscreen(int camid, GraphicsState window, State * state)
+int 
+renderOnscreen(int camid, GraphicsState window, State * state)
 {
 	return 0;
 }
--- src/utilOsmesa.c
+++ src/utilOsmesa.c
@@ -4,30 +4,33 @@
 #include "util.h"
 #include "stdio.h"
 
-// create OpenGL context/window
-int initOpenGL(OSMesaContext * ctx, void **buffer)
+// create OpenGL context / window
+int 
+initOpenGL(OSMesaContext * ctx, void **buffer)
 {
 	*buffer = malloc(10000000 * sizeof(unsigned char));
-	// create context
-	*ctx = OSMesaCreateContextExt(GL_RGBA, 24, 8, 8, 0);
+	//create context
+		* ctx = OSMesaCreateContextExt(GL_RGBA, 24, 8, 8, 0);
 	if (!*ctx)
 		mju_error("OSMesa context creation failed");
 
-	// make current
-	if (!OSMesaMakeCurrent(*ctx, *buffer, GL_UNSIGNED_BYTE, 800, 800))
+	//make current
+		if (!OSMesaMakeCurrent(*ctx, *buffer, GL_UNSIGNED_BYTE, 800, 800))
 		mju_error("OSMesa make current failed");
 	return 0;
 }
 
-// close OpenGL context/window
-int closeOpenGL(OSMesaContext ctx, void *buffer)
+//close OpenGL context / window
+int 
+closeOpenGL(OSMesaContext ctx, void *buffer)
 {
 	free(buffer);
 	OSMesaDestroyContext(ctx);
 	return 0;
 }
 
-int renderOnscreen(int camid, GraphicsState window, State * state)
+int 
+renderOnscreen(int camid, GraphicsState window, State * state)
 {
 	return 0;
 }
