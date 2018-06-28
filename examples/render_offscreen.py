4ad575a09204b7f9dae807dca8b85dcc1b7d64ed
Ethan Brooks <ethanabrooks@gmail.com>
2018-06-28 09:56:04 -0400

    .venv

--- examples/render_offscreen.py
+++ examples/render_offscreen.py
@@ -5,11 +5,41 @@ import argparse
 from PIL import Image
 
 import mujoco
+import subprocess
+import os
 
 parser = argparse.ArgumentParser()
 parser.add_argument('height', type=int)
 parser.add_argument('width', type=int)
+parser.add_argument('--video-path', type=str, default='build/video.mp4')
 args = parser.parse_args()
+
+command = [    'ffmpeg',
+          '-nostats',
+          '-loglevel',
+          'error',  # suppress warnings
+          '-y',
+          # '-r',
+          # '%d' % self.frames_per_sec,
+
+          # input
+          '-f',
+          'rawvideo',
+          '-s:v',
+          '{}x{}'.format(args.height, args.width),
+          '-pix_fmt',
+          'rgb32',
+          '-i',
+          '-',  # this used to be /dev/stdin, which is not Windows-friendly
+
+          # output
+          '-vcodec',
+          'libx264',
+          '-pix_fmt',
+          'yuv420p',
+          args.video_path]
+subprocess.Popen(command, stdin=subprocess.PIPE,
+                 preexec_fn=os.setsid)  # TODO: do we need this?
 
 print(args.height, args.width)
 sim = mujoco.Sim('xml/humanoid.xml', height=args.height, width=args.width)
